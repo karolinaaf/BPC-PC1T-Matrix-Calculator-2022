c7e91f60362b17963cdbd536dffd334bd60b6068
karolinaaf <110686580+karolinaaf@users.noreply.github.com>
2022-10-15 19:05:44 +0200

    Added Visual Studio CMake project
    pls run

---
+++ MaticovaKalkulacka.cpp
@@ -0,0 +1,12 @@
+﻿// MaticovaKalkulacka.cpp : Defines the entry point for the application.
+//
+
+#include "MaticovaKalkulacka.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
