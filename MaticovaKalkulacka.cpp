f69eb57c453227bb26a43fd722f0af0c824bf164
karolinaaf <110686580+karolinaaf@users.noreply.github.com>
2022-11-30 15:18:35 +0100

    File handling

--- MaticovaKalkulacka.cpp
+++ MaticovaKalkulacka.cpp
@@ -2,14 +2,29 @@
 //
 
 #include "MaticovaKalkulacka.h"
+#include "input.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	struct Matice {
+		int sloupce, radky;
+		double* data;
+	};
+	FILE* fr, * fw;
 
-	printf("Hello GitHub.\n");
+	char fileName[64];
+	char inputFilePath[] = "D://";
+	printf("Zadejte nazev souboru:");
+	scanf("%64s", fileName);
+	strcat(inputFilePath, fileName);
+	if ((fr = fopen(inputFilePath, "r")) == NULL) {
+		printf("Chyba pri otevirani %s\n", inputFilePath);
+		return 0;
+	}
 
+	if (fclose(fr) == EOF) printf("Chyba pri zavirani %s\n", inputFilePath);
+	if (fclose(fw) == EOF) printf("Chyba pri zavirani souboru.\n");
 	return 0;
 }
